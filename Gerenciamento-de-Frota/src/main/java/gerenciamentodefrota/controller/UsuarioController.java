5218166cad52d8856da2667b61211d7146090667
Vagner Oliveira Cotrim <vagner_cotrim@yahoo.com.br>
2013-08-19 17:55:48 -0400

    Adicionando recurso cadastrar usuário no controler Usuario;

--- Gerenciamento-de-Frota/src/main/java/gerenciamentodefrota/controller/UsuarioController.java
+++ Gerenciamento-de-Frota/src/main/java/gerenciamentodefrota/controller/UsuarioController.java
@@ -3,20 +3,50 @@ package gerenciamentodefrota.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import gerenciamentodefrota.annotation.Transacional;
+import gerenciamentodefrota.dao.FuncionarioDAO;
 import gerenciamentodefrota.dao.UsuarioDAO;
 import gerenciamentodefrota.model.Usuario;
 import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.Validator;
 
 @Resource
 public class UsuarioController {
 
 	private UsuarioDAO usuarioDAO;
+	private Validator validator;
+	private FuncionarioDAO funcionarioDAO;
+	private Result result;
 
-	public UsuarioController(UsuarioDAO usuarioDAO) {
+	public UsuarioController(UsuarioDAO usuarioDAO, FuncionarioDAO funcionarioDAO, Validator validator, Result result) {
 		this.usuarioDAO = usuarioDAO;
+		this.funcionarioDAO = funcionarioDAO;
+		this.validator = validator;
+		this.result = result;
 	}
-	
+
+	@Get
+	@Path(value = "/funcionario/{id}/usuario/novo", priority = Path.HIGHEST)
+	public void novo(Long id) {
+		result.include("funcionario", funcionarioDAO.busca(id));
+	}
+
+	@Transacional
+	@Post("/usuario/salvar")
+	public void salva(final Usuario usuario) {
+		usuario.setFuncionario(funcionarioDAO.busca(usuario.getFuncionario().getId()));
+		validator.validate(usuario);
+		
+		validator.onErrorRedirectTo(this).novo(usuario.getFuncionario().getId());
+		
+		usuarioDAO.adiciona(usuario);
+		result.redirectTo(this).lista();
+	}
+
 	@Get("/usuario")
 	public List<Usuario> lista() {
 		try {
@@ -25,5 +55,5 @@ public class UsuarioController {
 			return new ArrayList<Usuario>();
 		}
 	}
-	
+
 }
