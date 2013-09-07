1352c7217ca6157c6b7463e40278b33076710eaa
vagnercotrim <vagner_cotrim@yahoo.com.br>
2013-09-06 22:48:36 -0400

    Adicionando tag jsp TagLinkPermission que exibe o link ou não se o usuário tem permissão de acesso.

---
+++ Gerenciamento-de-Frota/src/main/java/gerenciamentodefrota/taglib/TagLinkPermission.java
@@ -0,0 +1,73 @@
+package gerenciamentodefrota.taglib;
+
+import gerenciamentodefrota.model.Perfil;
+import gerenciamentodefrota.model.UsuarioSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EnumType;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class TagLinkPermission extends TagSupport {
+
+	private static final long serialVersionUID = 7427251828658402481L;
+	private List<Perfil> perfis = new ArrayList<Perfil>();
+	private String link;
+	private String titulo;
+	private Boolean exibir = false;
+	private UsuarioSession usuarioSession;
+
+	@Override
+	public int doStartTag() throws JspException {
+		try {
+			JspWriter out = pageContext.getOut();
+
+			if (usuarioSession.isLogado() && hasAccess(usuarioSession.getUsuario().getPerfil())) {
+				out.println("<a href=\"" + link + "\">" + titulo + "</a>");
+			} else {
+				if (exibir) {
+					out.println("<span>" + titulo + "</span>");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return SKIP_BODY;
+	}
+
+	public void setPerfis(String perfis) {
+		String[] lista = perfis.split(",");
+		for (String string : lista) {
+			try {
+				Perfil perfil = EnumType.valueOf(Perfil.class, string.trim());
+				this.perfis.add(perfil);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public void setExibir(Boolean exibir) {
+		this.exibir = exibir;
+	}
+
+	public void setUsuarioSession(UsuarioSession usuarioSession) {
+		this.usuarioSession = usuarioSession;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	private Boolean hasAccess(Perfil perfil) {
+		return perfis.contains(perfil);
+	}
+}
--- Gerenciamento-de-Frota/src/main/webapp/WEB-INF/jsp/veiculo/lista.jsp
+++ Gerenciamento-de-Frota/src/main/webapp/WEB-INF/jsp/veiculo/lista.jsp
@@ -2,7 +2,7 @@
 	<div class="box-head">
 		<h2 class="left">Lista de veículos</h2>
 	</div>
-		
+
 	<div class="table">
 		<table width="100%" border="0" cellspacing="0" cellpadding="0">
 			<tr>
@@ -14,16 +14,16 @@
 				<th>Opções</th>
 			</tr>
 			<c:forEach items="${veiculoList}" var="veiculo">
-			<tr>
-				<td>${veiculo.descricao}</td>
-				<td>${veiculo.placa}</td>
-				<td>${veiculo.anoFabricacao}</td>
-				<td>${veiculo.anoModelo}</td>
-				<td><c:out value="${veiculo.propriedade}" /></td>
-				<td>
-					<a href="${pageContext.request.contextPath}/veiculo/${veiculo.id}">editar</a>
-				</td>
-			</tr>
+				<tr>
+					<td>${veiculo.descricao}</td>
+					<td>${veiculo.placa}</td>
+					<td>${veiculo.anoFabricacao}</td>
+					<td>${veiculo.anoModelo}</td>
+					<td><c:out value="${veiculo.propriedade}" /></td>
+					<td><a
+						href="${pageContext.request.contextPath}/veiculo/${veiculo.id}">editar</a>
+					</td>
+				</tr>
 			</c:forEach>
 		</table>
 	</div>
@@ -31,7 +31,9 @@
 </div>
 
 <content tag="breadcrumb">
-	<div class="small-nav">
-		<a href="${pageContext.request.contextPath}/veiculo/novo">Cadastrar veículo</a>
-	</div>
+<div class="small-nav">
+	<tag:linkpermission perfis="USUARIO,ADMINISTRADOR"
+		link="${pageContext.request.contextPath}/veiculo/novo" exibir="true"
+		titulo="Cadastrar veículo" usuarioSession="${usuarioSession}" />
+</div>
 </content>
